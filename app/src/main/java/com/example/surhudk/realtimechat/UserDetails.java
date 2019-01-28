4711c11e6be5582476694e16ea19d86fdd3a5a5a
LordSSK <surhudsk@gmail.com>
2019-01-28 19:53:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/surhudk/realtimechat/UserDetails.java
@@ -0,0 +1,10 @@
+package com.example.surhudk.realtimechat;
+
+/**
+ * Created by SurhudK on 10/17/2017.
+ */
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
